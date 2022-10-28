c4cb24c1d0618f7ddb5316033bfc44f4bb6ab1f2
symon254 <79975499+symon254@users.noreply.github.com>
2022-10-28 12:05:33 +0300

    build:
    change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const workoutRoutes = require("./routes/workouts");
+//const workoutRoutes = require("./routes/workouts");
 
 //express app
 const app = express();
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 });
 
 //routes
-app.use("api/workouts", workoutRoutes);
+app.use("api/users");
 
 //connect to db
 mongoose
