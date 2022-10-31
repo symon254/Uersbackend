1cb32161adb9775ad4447b9b82e0697648a8ad85
symon254 <79975499+symon254@users.noreply.github.com>
2022-10-31 11:15:19 +0300

    feat: adding auth
    auth routes controles and model

---
+++ Routes/Users.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { loginUser, signupUser } = require("../controlers/userController");
+
+const router = express.Router();
+
+//login route
+router.post("/login", loginUser);
+
+//signup route
+router.post("/signup", signupUser);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const contactsRoutes = require("./Routes/Contacts");
+const userRoutes = require("./Routes/Users");
 
 //express app
 const app = express();
@@ -15,6 +16,7 @@ app.use((req, res, next) => {
 
 //routes
 app.use("/api/contacts", contactsRoutes);
+app.use("/api/user", userRoutes);
 // conect to db
 mongoose
     .connect(process.env.MONGO_DB_URI, {
@@ -33,33 +35,3 @@ mongoose
     .catch((error) => {
         console.log(error);
     });
-
-// //middleware
-// app.use(express.json());
-// app.use(cors());
-// app.use((req, res, next) => {
-//     console.log(req.path, res.method);
-//     next();
-// });
-
-// //routes
-// app.use("api/users");
-
-// //connect to db
-// mongoose
-//     .connect(process.env.MONGO_URI, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//     })
-//     .then(() => {
-//         //listen for requests
-//         app.listen(process.env.PORT, () => {
-//             console.log(
-//                 "connected to database and listen on port ",
-//                 process.env.PORT
-//             );
-//         });
-//     })
-//     .catch((error) => {
-//         console.log(error);
-//     });
